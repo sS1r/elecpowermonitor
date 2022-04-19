94cd025cff1422880939a40f37ce68c2a813496d
ss1r <you@example.com>
2022-04-19 22:40:56 +0300

    Add main function

--- src/main.py
+++ src/main.py
@@ -2,12 +2,20 @@ from fgApi import fingridApi
 from plotdata import plot_data
 
 API_URL = 'https://api.fingrid.fi'
-KEYFILE = '../key.txt'
+KEYFILE = './key.txt'
 
-with open(KEYFILE) as f: api_key = f.readline()
-api = fingridApi(url=API_URL, api_key=api_key)
+def main():
+	with open(KEYFILE) as f:
+		api_key = f.readline()
+	api = fingridApi(url=API_URL, api_key=api_key)
 
-if api.server_up():
-	if api.send_request():
-		data = api.get_data()
-		plot_data(data)
+	if api.server_up():
+		if api.send_request():
+			data = api.get_data()
+			plot_data(data)
+	else:
+		print("Fingrid API down :(")
+
+
+if __name__ == "__main__":
+	main()
