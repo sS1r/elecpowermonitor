69221e130894b6a2aacada1ee82f6ed81b9a0c31
ss1r <you@example.com>
2023-01-14 23:15:23 +0200

    Add command line interface

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import click
+
 from fgApi import fingridApi
 from entsoeApi import entsoeApi
 
@@ -8,25 +10,71 @@ API_URL_ENTSOE = 'https://web-api.tp.entsoe.eu/api?'
 KEYFILE_FG = './key_fg.txt'
 KEYFILE_ENTSOE = './key_entsoe.txt'
 
-def main():
-	with open(KEYFILE_FG) as f:
-		api_key_fg = f.readline()
-	api_fg = fingridApi(url=API_URL_FG, api_key=api_key_fg)
+DATA_FORMAT_OPTIONS = ['CSV', 'XLSX', 'JSON', 'TXT']
 
-	with open(KEYFILE_ENTSOE) as f:
-		api_key_entsoe = f.readline()
-	api_entsoe = entsoeApi(url=API_URL_ENTSOE, api_key=api_key_entsoe)
 
-	if api_fg.server_up():
-		if api_fg.send_request():
-			data = api_fg.get_data()
-			plot_data(data)
+'''
+Opens API key and sends request to fetch data from it
+
+Returns: API object if successful, None otherwise
+'''
+def run_api(keyfile, url, api_class):
+	with open(keyfile) as f:
+		api_key = f.readline()
+	api = api_class(url=url, api_key=api_key)
+
+	if api.server_up():
+		if api.send_request():
+			return api
+		else:
+			print("Request to " + api.name +  " failed!")
 	else:
-		print("Fingrid API down :(")
+		print(api.name + " API down!")
+	return None
 
-	if api_entsoe.send_request():
-		data = api_entsoe.get_data()
-	plot_elecprice(data)
+@click.group()
+def main():
+	pass
+
+@main.group()
+def price():
+	pass
+
+@main.group()
+def fingrid():
+	pass
+
+@price.command(name="plot")
+@click.option('--hide', required=False,  is_flag=True, help='Do not display the interactive plot')
+@click.option('--save', required=False,  nargs=1, type=str, help='Save the image with given file name')
+def plot(hide, save):
+	api = run_api(keyfile=KEYFILE_ENTSOE, url=API_URL_ENTSOE, api_class=entsoeApi)
+	if api:
+		plot_elecprice(api.get_data())
+
+@price.command(name="fetch")
+@click.option('--format',   required=False, nargs=1, type=click.Choice(DATA_FORMAT_OPTIONS, case_sensitive=False), help='Data format')
+@click.option('--filename', required=False, nargs=1, type=str, help='Filename')
+def fetch(format, filename):
+	api = run_api(keyfile=KEYFILE_ENTSOE, url=API_URL_ENTSOE, api_class=entsoeApi)
+	if api:
+		api.save_data(filename, format)
+
+@fingrid.command(name="plot")
+@click.option('--hide', required=False,  is_flag=True, help='Do not display the interactive plot')
+@click.option('--save', required=False,  nargs=1, type=str, help='Save the image with given file name')
+def plot(hide, save):
+	api = run_api(keyfile=KEYFILE_FG, url=API_URL_FG, api_class=fingridApi)
+	if api:
+		plot_data(api.get_data())
+
+@fingrid.command(name="fetch")
+@click.option('--format',   required=False, nargs=1, type=click.Choice(DATA_FORMAT_OPTIONS, case_sensitive=False), help='Data format')
+@click.option('--filename', required=False, nargs=1, type=str, help='Filename')
+def fetch(format, filename):
+	api = run_api(keyfile=KEYFILE_FG, url=API_URL_FG, api_class=fingridApi)
+	if api:
+		api.save_data(filename, format)
 
 if __name__ == "__main__":
 	main()
