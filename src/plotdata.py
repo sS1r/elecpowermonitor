e2a459dbf90ece8d97336dd2b18f85cd76d01439
ss1r <you@example.com>
2023-04-23 11:11:08 +0300

    - Stairs plot is used in price plot - Minor grid is visible - Fixed issue where tool crashed if API did not contain data up to current time

--- src/plotdata.py
+++ src/plotdata.py
@@ -50,30 +50,39 @@ def plot_elecprice(api, hide=False, fname=None):
 	data = api.get_data()
 	now = datetime.now()
 	nowstr = now.strftime("%d.%m.%Y klo %H:%M")
+	nearest_available = False
 	for t in data.data.index:
 		if t.replace(tzinfo=None) > now:
 			i_nearest = data.data.index.get_loc(t) - 1
 			t_nearest = data.data.index[i_nearest].replace(tzinfo=None)
 			t_next = t
+			nearest_available = True
 			break
-	pricenow = data.data["price"][i_nearest]
-	notestr = "Hinta {}:\n{:.3f} EUR/kWh".format(nowstr, pricenow)
+
+	if nearest_available:
+		pricenow = data.data["price"][i_nearest]
+		notestr = "Hinta {}:\n{:.3f} EUR/kWh".format(nowstr, pricenow)
 
 	fig, ax = plt.subplots(figsize=(19.2, 10.8), dpi=100.0)
 
-	plt.plot(data.data.index, data.data["price"], label="Sähkön hinta")
-	plt.axvline(x=now,      color='r', linestyle='--', label="Nyt")
-	plt.axhline(y=pricenow, color='b', linestyle='--', label="Nykyinen hinta")
-	plt.axvspan(t_nearest, t_next, color='green', alpha=0.2, label='Nykyhinta voimassa')
+
+	plt.minorticks_on()
+	plt.stairs(data.data["price"], data.data.index.to_list() + [data.data.index[-1]], label="Sähkön hinta", linewidth=2)
+	plt.axvline(x=now,             color='r', linestyle='--', label="Nyt")
+
+	if nearest_available:
+		plt.axhline(y=pricenow,        color='b', linestyle='--', label="Nykyinen hinta")
+		plt.axvspan(t_nearest, t_next, color='g', alpha=0.2,      label='Nykyhinta voimassa')
+		ax.text(now + timedelta(minutes=60), pricenow * 1.1, notestr, fontsize=12)
 
 	ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("%H:%M"))
-	ax.text(now + timedelta(minutes=60), pricenow * 1.1, notestr, fontsize=12)
+	ax.xaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator(6))
 
 	plt.legend(loc='upper right')
-	plt.title("Pörssisähkön hinta", fontsize=20)
+	plt.title("Pörssisähkön hinta (ALV {:.1f}%)".format(api.vat), fontsize=20)
 	plt.xlabel("Aika", fontsize=14)
 	plt.ylabel("Hinta (EUR/kWh)", fontsize=14)
-	plt.grid(True)
+	plt.grid(True, which="both")
 
 	if fname:
 		plt.savefig(fname)
